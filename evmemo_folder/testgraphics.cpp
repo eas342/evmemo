05b1b9581b8d4f759372f9a11f42dc97cf1a0968
Everett Schlawin <bokonon@bokonons-ibook-g4.local>
2010-05-07 17:29:28 -0400

    It looked like the title bar was covered up about half the time when I just load both programs quickly. Now with sleep 0.1 before and after blocker.txt it seems like the title bar is covered up all the time.

--- evmemo_folder/testgraphics.cpp
+++ evmemo_folder/testgraphics.cpp
@@ -6,7 +6,9 @@ int main()
 {
 
   system("open graphics/medical/anatomy/fibula.gif");
+  system("sleep 0.1");
   system("open graphics/blocker.txt");
+  system("sleep 0.1");
   system("open /Applications/Utilities/Terminal.app");
   return 0;
 
