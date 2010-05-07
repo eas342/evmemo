3943e3b2cee8b2c849e293c25863756d450c6da9
Everett Schlawin <bokonon@bokonons-ibook-g4.local>
2010-05-07 17:26:36 -0400

    Added testgraphics.cpp because I'm having trouble blocking the title of images. This simple program is designed to help me out.

---
+++ evmemo_folder/testgraphics.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+  system("open graphics/medical/anatomy/fibula.gif");
+  system("open graphics/blocker.txt");
+  system("open /Applications/Utilities/Terminal.app");
+  return 0;
+
+}
