5734af9ab7ce6e28ec79c852435084ceca65dece
Everett Schlawin <bokonon@bokonons-ibook-g4.local>
2010-05-07 17:32:50 -0400

    0.05 seconds seems to do the trick.

--- evmemo_folder/testgraphics.cpp
+++ evmemo_folder/testgraphics.cpp
@@ -6,9 +6,9 @@ int main()
 {
 
   system("open graphics/medical/anatomy/fibula.gif");
-  system("sleep 0.1");
+  system("sleep 0.05");
   system("open graphics/blocker.txt");
-  system("sleep 0.1");
+//  system("sleep 0.05");
   system("open /Applications/Utilities/Terminal.app");
   return 0;
 
